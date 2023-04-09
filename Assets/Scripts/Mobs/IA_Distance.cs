a1dea6b3613865ce8facca839c75646fb4b46e8a
Neroxgd <darkaux69@gmail.com>
2023-04-09 04:30:09 +0200

    blorp

--- Assets/Scripts/Mobs/IA_Distance.cs
+++ Assets/Scripts/Mobs/IA_Distance.cs
@@ -8,10 +8,12 @@ public class IA_Distance : IA
 {
     [SerializeField] private GameObject projectil;
     [SerializeField] private float speedProjectil = 1f;
+    [SerializeField] private Color colorProjectil;
 
     protected override IEnumerator AttackPlayer()
     {
         GameObject proj = Instantiate(projectil, transform.position, Pratique.LookAt2D(projectil.transform.rotation, DirectionToPlayer), transform);
+        proj.GetComponentInChildren<SpriteRenderer>().color = colorProjectil;
         proj.transform.DOMove(proj.transform.position + proj.transform.up * 50, speedProjectil)
         .SetSpeedBased(true)
         .SetEase(Ease.Linear)
