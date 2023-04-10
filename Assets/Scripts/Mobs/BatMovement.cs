1f9d19e304e99fb1f03ab9e6b37ddf7f7934366d
Neroxgd <darkaux69@gmail.com>
2023-04-10 04:40:41 +0200

    cave

--- Assets/Scripts/Mobs/BatMovement.cs
+++ Assets/Scripts/Mobs/BatMovement.cs
@@ -1,37 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using DG.Tweening;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using DG.Tweening;
 
-public class BatMovement : MonoBehaviour
-{
-    [SerializeField] private Transform[] PointDeplacements;
-    private int indexDeplacement = 0;
-    [SerializeField] private float speedBat = 1;
-    [SerializeField] private PlayerLife _playerLife;
-    [SerializeField] private float batDamage = 10;
-    [SerializeField] private Ease ease = Ease.InOutCubic;
-    void Start()
-    {
-        transform.position = PointDeplacements[0].position;
-        Fly();
-    }
+// public class BatMovement : MonoBehaviour
+// {
+//     [SerializeField] private Transform[] PointDeplacements;
+//     private int indexDeplacement = 0;
+//     [SerializeField] private float speedBat = 1;
+//     [SerializeField] private PlayerLife _playerLife;
+//     [SerializeField] private float batDamage = 10;
+//     [SerializeField] private Ease ease = Ease.InOutCubic;
+//     void Start()
+//     {
+//         transform.position = PointDeplacements[0].position;
+//         Fly();
+//     }
 
-    private void Fly()
-    {
-        indexDeplacement++;
-        transform.DOMove(PointDeplacements[indexDeplacement].position, speedBat)
-        .SetEase(ease)
-        .OnComplete(Fly)
-        .SetSpeedBased(true)
-        .SetDelay(Random.Range(3, 10));
-        if (indexDeplacement == PointDeplacements.Length - 1)
-            indexDeplacement = 0;
-    }
+//     private void Fly()
+//     {
+//         indexDeplacement++;
+//         transform.DOMove(PointDeplacements[indexDeplacement].position, speedBat)
+//         .SetEase(ease)
+//         .OnComplete(Fly)
+//         .SetSpeedBased(true)
+//         .SetDelay(Random.Range(3, 10));
+//         if (indexDeplacement == PointDeplacements.Length - 1)
+//             indexDeplacement = 0;
+//     }
 
-    // void OnTriggerEnter2D(Collider2D other)
-    // {
-    //     if (other.CompareTag("Player"))
-    //         _playerLife.TakeDamage(batDamage);
-    // }
-}
+//     // void OnTriggerEnter2D(Collider2D other)
+//     // {
+//     //     if (other.CompareTag("Player"))
+//     //         _playerLife.TakeDamage(batDamage);
+//     // }
+// }
